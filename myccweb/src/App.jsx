6740f604e2ae1d8933f79a9fb121c8f651e9d564
Ameer Hamza <151500343+Berrymalik123@users.noreply.github.com>
2026-01-19 18:36:56 +0500

    Add files via upload

---
+++ myccweb/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import Header from './components/Header'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import Tester from './components/Tester'
+
+function App() {
+  return (
+    <>
+    {/* <About /> */}
+    {/* <Header /> */}
+    {/* <Navbar />
+    <Tester /> */}
+
+    </>
+  )
+}
+
+export default App
